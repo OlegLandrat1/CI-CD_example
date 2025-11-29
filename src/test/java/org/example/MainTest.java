685da54ab8f389a915ef021365cf921f83786d34
jktu <landrat@bk.ru>
2025-11-29 13:06:11 +0500

    first commit

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @org.junit.jupiter.api.Test
+    void main() {
+        int val = 5;
+        assertEquals(5, val, "val mach");
+    }
+}
